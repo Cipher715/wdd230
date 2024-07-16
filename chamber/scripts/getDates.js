4dac2f58696b2dfd504177c0a60a729f2c2247ad
Cipher715 <atsushi.karino@gmail.com>
2024-07-17 00:27:39 +0900

    W4 chamber

--- chamber/scripts/getDates.js
+++ chamber/scripts/getDates.js
@@ -1,3 +1,9 @@
-document.querySelector('#year').textContent = new Date().getFullYear();
+const currentdate = new Date();
+const dateTime = (currentdate.getMonth()+1) + "/" + currentdate.getDate() 
++ "/" + currentdate.getFullYear() + " | "  
++ currentdate.getHours() + ":" 
++ currentdate.getMinutes() + ":" + currentdate.getSeconds();
 
+document.querySelector('#year').textContent = currentdate.getFullYear();
 document.querySelector('#lastModified').textContent =  document.lastModified;
+document.querySelector('#timestamp').textContent = dateTime;
