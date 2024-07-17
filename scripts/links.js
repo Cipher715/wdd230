5b3168920840144ea2df2a39a3d9f09461e48e85
Cipher715 <atsushi.karino@gmail.com>
2024-07-18 01:48:33 +0900

    W5 weather

--- scripts/links.js
+++ scripts/links.js
@@ -1,11 +1,36 @@
 const baseURL = "https://cipher715.github.io/wdd230/";
 const linksURL = "https://cipher715.github.io/wdd230/data/links.json";
-
+const learnings = document.querySelector('#learnings');
 
 async function getLinks() {
     const result = await fetch(linksURL);
     const data = await result.json();
-    console.log(data);
+    displayLinks(data.weeks);
+}
+
+function displayLinks(data){
+    data.forEach(weeks=> {
+        let li = document.createElement('li');
+        li.textContent = `${weeks.week}: `
+        let index = 0;
+        weeks.links.forEach(link => {
+            index ++;
+            let a = document.createElement('a');
+            let p = document.createElement('span');
+    
+            a.setAttribute('href', link.url);
+            a.setAttribute('alt', link.title);
+            a.textContent = link.title;
+            p.textContent = " | "
+            li.appendChild(a);
+            if (index != weeks.links.length){
+                li.appendChild(p);
+            }
+            //li.appendChild(p);
+
+        });
+        learnings.appendChild(li);
+    })
 }
 
 getLinks();
